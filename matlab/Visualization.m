034deb09775d64d9bdf7b40f86feee0d98498136
Frederike <frederike.duembgen@epfl.ch>
2018-07-19 23:03:14 +0200

    Generalized Matlab script.

--- matlab/Visualization.m
+++ matlab/Visualization.m
@@ -1,22 +1,22 @@
 clearvars; clc;
 
 img = [1 2 3; 4 5 6; -7 8 9; -1 -2 3]
+%img = [1 2 3; 4 5 6; -7 8 9]
+
 S = size(img);
-k = [[-1 2]; [-3 -4]]
+n = numel(img);
+k = [[-1 2]; [-3 -4]];
 
 %%
 disp('~~~~from conv2d:')
-conv2(img, k, 'valid')
-
-
+result_orig = conv2(img, k, 'valid')
 
 %%
 k_otf = psf2otf(k, S);
 
 v2 = fft2(img).*k_otf; 
 disp('~~~~from psf2otf:')
-ifft2(v2)
-
+result_psf2otf = ifft2(v2)
 
 
 %%
@@ -24,33 +24,28 @@ disp('~~~~from big matrix multiply:')
 imgT = img';
 imgVec = imgT(:);
 
-kFilled = [-1 2 0 -3 -4 0 0 0 0 0 0 0];
+kFilled = zeros(1, n);
+kFilled(1:6) = [-4 -3 0 2 -1 0];
 
-for m = 1:12
+for m = 1:n
     kCirc(m, :) = circshift(kFilled, m-1);
 end
 
-res = kCirc * imgVec;
-res = reshape(res, [3, 4])'
+result_time = kCirc * imgVec;
+result_time = reshape(result_time, S(2), S(1))'
 
 
 
 %%
-kFilled = [-1 2 0 -3 -4 0 0 0 0 0 0 0];
+kFilled = zeros(1, n);
+kFilled(1:6) = [-1 2 0 -3 -4 0];
 
-for m = 1:12
+for m = 1:n
     kCirc(m, :) = circshift(kFilled, m-1);
 end
 
-kCirc
-
 disp('~~~~from big matrix DFT:')
-kF = fft2((kCirc/12)) %is nxn, only as n entries
-imgF = fft(imgVec) % 1D vectorized image.
-res = flip(ifft(kF * imgF)); 
-res = reshape(res, [3, 4])'
-
-fId = fopen('kFreal.txt', 'w');
-fprintf(fId,'%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f  \n',real(kF));
-fId = fopen('kFimag.txt', 'w');
-fprintf(fId,'%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f  \n',imag(kF));
+kF = fft2((kCirc/n)); 
+imgF = fft(imgVec);
+result_fourier = flip(ifft(kF * imgF)); 
+result_fourier = reshape(result_fourier, S(2), S(1))'
